13dc1389d6330434f8e2f8fef4c4d6fa3fef7cbd
Filipe <filipester@gmail.com>
2025-02-26 20:24:59 -0300

    tests

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -7,19 +7,21 @@ import numpy as np
 df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['height'] = df['height'] / 100
+df['overweight'] = ((df['weight'] / (df['height'] ** 2)) > 25).astype(int)
 
 # 3
-
+df['cholesterol'] = df.apply(lambda x : 0 if x['cholesterol'] == 1 else 1, axis=1).astype(int)
+df['gluc'] = df.apply(lambda x : 0 if x['gluc'] == 1 else 1, axis=1).astype(int)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, id_vars='cardio', value_vars = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat['cardio'].value_counts()
     
 
     # 7
